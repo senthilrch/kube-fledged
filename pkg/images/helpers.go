b799a21fd2c0a90df296444ef7e6b0f58f0f6b1b
senthilrch <senthilrch@gmail.com>
2018-11-21 07:48:36 -0500

    termination log of image delete

--- pkg/images/helpers.go
+++ pkg/images/helpers.go
@@ -165,8 +165,8 @@ func newImageDeleteJob(imagecache *fledgedv1alpha1.ImageCache, image string, hos
 						{
 							Name:    "docker-client",
 							Image:   dockerclientimage,
-							Command: []string{"/usr/bin/docker"},
-							Args:    []string{"image rm " + image + " > /dev/termination-log"},
+							Command: []string{"/bin/bash"},
+							Args:    []string{"-c", "exec /usr/bin/docker image rm " + image + " > /dev/termination-log 2>&1"},
 							VolumeMounts: []corev1.VolumeMount{
 								{
 									Name:      "docker-sock",
