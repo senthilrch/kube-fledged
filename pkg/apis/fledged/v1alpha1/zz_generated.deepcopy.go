55f6e20bfe7e759a92d0d20986e202a6aba652a3
senthilrch <senthilrch@gmail.com>
2020-01-23 18:30:43 +0800

    migrating to go mod from dep

--- pkg/apis/fledged/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/fledged/v1alpha1/zz_generated.deepcopy.go
@@ -85,7 +85,7 @@ func (in *ImageCache) DeepCopyObject() runtime.Object {
 func (in *ImageCacheList) DeepCopyInto(out *ImageCacheList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]ImageCache, len(*in))
